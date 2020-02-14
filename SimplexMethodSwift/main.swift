a5fe100a63820a291939d7729f5f658b5e3bfc31
Sergey <seregakubakh@gmail.com>
2020-02-14 16:28:18 +0200

    Initial Commit

---
+++ SimplexMethodSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimplexMethodSwift
+//
+//  Created by w0ter on 2/14/20.
+//  Copyright © 2020 w0ter. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
